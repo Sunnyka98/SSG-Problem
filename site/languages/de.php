3a4fe19c5594473aa47858cf15fb536e8e5ab945
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2022-06-10 09:43:38 +0200

    Add Content and Languages

---
+++ site/languages/de.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'code' => 'de',
+  'default' => true,
+  'direction' => 'ltr',
+  'locale' => 'de_DE',
+  'name' => 'Deutsch',
+  'translations' => [
+        "pageLang" => "Diese Seite ist auf deutsch"
+    ],
+    'url' => '/de'
+];
---
+++ site/languages/en.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'code' => 'en',
+    'direction' => 'ltr',
+    'locale' => [
+        'LC_ALL' => 'en_GB'
+    ],
+    'name' => 'English',
+    'translations' => [
+        'pageLang' => "This Page is on English"
+    ],
+    'url' => '/en'
+];
